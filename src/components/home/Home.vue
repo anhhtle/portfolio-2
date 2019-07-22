993b66269c6b9de490baacbebc1e3f76bd6078e4
Anh T Le <ahtle@stanford.edu>
2019-07-21 21:36:46 -0700

    initial

---
+++ src/components/home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="Home">
+    <SectionOne />
+  </div>
+</template>
+
+<script>
+import SectionOne from './SectionOne';
+
+export default {
+  components: {
+    SectionOne,
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/home/SectionOne.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="SectionOne">
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="sass" scoped>
+#SectionOne
+    height: 100vh;
+    width: 100%;
+    background-image: url(../../assets/images/sf-bg.png)
+img
+    background-size: cover;
+</style>
