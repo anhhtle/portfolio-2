05941b6ac47c360226820c93fdd782606d657fef
Anh T Le <ahtle@stanford.edu>
2019-08-11 18:51:25 -0700

    ok

--- src/components/home/SectionThree.vue
+++ src/components/home/SectionThree.vue
@@ -3,8 +3,11 @@
     <h3 class="section-header">Chose your Coder</h3>
 
     <div class="row">
-      <div class="col-sm-6">
-        <img class="img-border" src="https://dcp.org/wp-content/uploads/2018/11/person-placeholder-e1543539209738.jpg" />
+      <div class="col-sm-6 img-container">
+        <div class="img-border" @mouseover="mouseover = true" @mouseleave="mouseover = false">
+          <img v-if="!mouseover" src="../../assets/images/dino-1.png" />
+          <img v-else src="../../assets/images/dino-2.png" />
+        </div>
       </div>
 
 
@@ -14,16 +17,25 @@
 
 <script>
 export default {
+  data() {
+    return {
+      mouseover: false,
+    }
+  },
 }
 </script>
 
 <style lang="sass" scoped>
 #SectionThree
   background: linear-gradient(180deg,#1a1514,#413561);
-  .img-border
-    border: 5px solid #fff;
-    box-shadow: 10px 10px 0 rgba(0,0,0,.75);
-  img
-    width: 200px;
-    height: 200px;
+  .img-container
+    display: flex;
+    justify-content: center;
+    .img-border
+      border: 5px solid #fff;
+      box-shadow: 10px 10px 0 rgba(0,0,0,.75);
+      width: fit-content;
+      img
+        width: 200px;
+        height: 200px;
 </style>
