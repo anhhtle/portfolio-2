f4b95551bd6c61241c09947af21eb8b49dcd3f4b
Anh Le <ahtle@stanford.edu>
2021-02-12 14:31:05 -0800

    ok

--- src/components/home/SectionOne.vue
+++ src/components/home/SectionOne.vue
@@ -11,38 +11,41 @@ export default {
 
 <style lang="scss" scoped>
 #SectionOne {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  width: 100%;
-  background-image: url(../../assets/images/sf-bg.png);
-  background-size: cover;
-  image-rendering: pixelated;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    width: 100%;
+    background-image: url(../../assets/images/sf-bg.png);
+    background-size: cover;
+    image-rendering: pixelated;
 }
 .pulse {
-  animation: pulse infinite 4s ease-in-out;
+    animation: pulse infinite 4s ease-in-out;
 }
 @keyframes pulse {
-  0% 
-    {filter: contrast(1);}
-  60% 
-    {filter: contrast(3);}
-  100%
-    {filter: contrast(1);}
+    0% 
+        {filter: contrast(1);}
+    60% 
+        {filter: contrast(3);}
+    100%
+        {filter: contrast(1);}
 }
 
 /* Medium devices (landscape tablets, 768px and up) */
 @media only screen and (max-width: 768px) {
-  .pulse {
-    width: 500px;
-  }
+    .pulse {
+        width: 500px;
+    }
 }
 
 /* Small devices (portrait tablets and large phones, 600px and up) */
 @media only screen and (max-width: 600px) {
-  .pulse {
-    width: 350px;
-  }
+    #SectionOne {
+        height: 400px;
+    }
+    .pulse {
+        width: 350px;
+    }
 }
 </style>
